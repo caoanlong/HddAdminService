17b58eabe6b89d8a1ee1054a968e26c7fc92fcb1
Caoanlong <caoanlong@gmail.com>
2018-06-11 14:24:00 +0800

    ggg

--- routes/sys_user.js
+++ routes/sys_user.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const Sequelize = require('sequelize')
-const jwt = require('jwt-simple')
 const snowflake = require('../utils/snowflake')
 const genPassword = require('../utils/cryptoPassword')
 
