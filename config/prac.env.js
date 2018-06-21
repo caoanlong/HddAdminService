b14b375aa07827a1dd5e6bcf2a5cd28f7e1c3da9
Caoanlong <caoanlong@gmail.com>
2018-06-21 15:15:02 +0800

    修改环境配置

---
+++ config/prac.env.js
@@ -0,0 +1,15 @@
+module.exports = {
+	port: 3001,
+	mysql: {
+		host: '192.168.0.28',
+		port: 33060,
+		user: 'hdd',
+		password: 'R84:5V736aKPJ9a3z>8VxJ8G',
+		database: 'hdd_v3'
+	},
+	redis: {
+		host: '192.168.1.48',
+		port: 6379,
+		password: '123456'
+	}
+}
---
+++ config/test.env.js
@@ -0,0 +1,15 @@
+module.exports = {
+	port: 3000,
+	mysql: {
+		host: '192.168.1.48',
+		port: 3306,
+		user: 'root',
+		password: 'We@123456',
+		database: 'hdd_v3_5_test'
+	},
+	redis: {
+		host: '192.168.1.48',
+		port: 6379,
+		password: '123456'
+	}
+}
