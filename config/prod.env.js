2f49a58179004a9b2dc62e49d79555d4a5285586
Cao Anlong <caoanlong@gmail.com>
2018-10-23 12:04:24 +0800

    Update prod.env.js

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,13 +3,13 @@ module.exports = {
 	mysql: {
 		host: '192.168.0.28',
 		port: 33060,
-		user: 'hdd',
-		password: 'R84:5V736aKPJ9a3z>8VxJ8G',
-		database: 'hdd_v3'
+		user: '',
+		password: '',
+		database: ''
 	},
 	redis: {
 		host: '192.168.0.28',
 		port: 6379,
-		password: '123456'
+		password: ''
 	}
 }
