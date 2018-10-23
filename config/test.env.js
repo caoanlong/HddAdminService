bee986468d5b102620be309106ca2e5c84b4dc2f
Cao Anlong <caoanlong@gmail.com>
2018-10-23 12:04:43 +0800

    Update test.env.js

--- config/test.env.js
+++ config/test.env.js
@@ -3,13 +3,13 @@ module.exports = {
 	mysql: {
 		host: '192.168.1.48',
 		port: 3306,
-		user: 'root',
-		password: 'We@123456',
-		database: 'hdd_v3_5_test'
+		user: '',
+		password: '',
+		database: ''
 	},
 	redis: {
 		host: '192.168.1.48',
 		port: 6379,
-		password: '123456'
+		password: ''
 	}
 }
