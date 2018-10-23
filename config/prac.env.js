58d21eed3253307aaaeeae4054b7cfcdebfd3b72
Cao Anlong <caoanlong@gmail.com>
2018-10-23 12:04:00 +0800

    Update prac.env.js

--- config/prac.env.js
+++ config/prac.env.js
@@ -3,13 +3,13 @@ module.exports = {
 	mysql: {
 		host: '192.168.0.28',
 		port: 33060,
-		user: 'hdd',
-		password: 'R84:5V736aKPJ9a3z>8VxJ8G',
+		user: '',
+		password: '',
 		database: 'hdd_v3'
 	},
 	redis: {
 		host: '192.168.1.48',
 		port: 6379,
-		password: '123456'
+		password: ''
 	}
 }
